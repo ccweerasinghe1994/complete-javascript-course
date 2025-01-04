34c3c7483226b6690cb5501052b588371311b6d6
wchamara <chamara.weerasinghe@my.sliit.lk>
2025-01-04 10:41:33 +0530

    add TypeScript variables and strict mode to JavaScript file

--- 01-Fundamentals-Part-1/starter/script.ts
+++ 01-Fundamentals-Part-1/starter/script.ts
@@ -1,12 +1,14 @@
-"use strict";
-let myName = "Sai";
+let myName: string = "Sai";
 console.log(myName);
 console.log("this is added ny thr browser");
 let age = 23; // Error: Cannot start with a number
 let userName = "Sai"; // Renamed to avoid reserved keyword and redeclaration
+
 console.log(userName);
 let PI = 3.14; // Error: Cannot use reserved keyword
+
 // when creating a variable, we can use the following rules:
+
 // 1. Use camelCase for naming variables
 // 2. Cannot start with a number
 // 3. Cannot use reserved keywords
@@ -14,11 +16,15 @@ let PI = 3.14; // Error: Cannot use reserved keyword
 // 5. Should be meaningful
 // 6. Cannot redeclare variables with the same name
 // 7. Cannot use reserved keywords
+
 // good examples
+
 let firstName = "Sai";
 let lastName = "Kiran";
 let country = "India";
+
 // bad examples
+
 let first_name = "Sai";
 // let last-name = "Kiran"; // Error: Cannot use special characters
 //  let 1country = "India"; // Error: Cannot start with a number
@@ -26,7 +32,7 @@ let first_name = "Sai";
 // let var = "Hello"; // Error: Cannot use reserved keywords
 // let break = "Hello"; // Error: Cannot use reserved keywords
 // let function = "Hello"; // Error: Cannot use reserved keywords
+
 // these are valid variables
 let $name = "Sai";
 let _name = "Sai";
-//# sourceMappingURL=script.js.map
